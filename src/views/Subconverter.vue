70f7d43ae329ef4bb3435b41812643c3b797ee7f
sub10086 <104071881+sub10086@users.noreply.github.com>
2022-06-29 11:20:57 +0800

    🐶cs2-3

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -255,7 +255,7 @@ export default {
           ClashR: "clashr",
           Surge2: "surge&ver=2",
         },
-        backendOptions: [{ value: "https://api.dingyue.gq/sub?" },label: "站长自用",
+        backendOptions: [{ value: "https://api.dingyue.gq/sub?" },{label: "站长自用",
                 value:
                   "https://api2.dingyue.gq/sub?"
               },],
