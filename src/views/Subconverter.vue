c38eea3e5bd27b91dd56d29da1c4f13dfb833953
sub10086 <104071881+sub10086@users.noreply.github.com>
2022-06-29 11:03:31 +0800

    cs失败

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -255,10 +255,7 @@ export default {
           ClashR: "clashr",
           Surge2: "surge&ver=2",
         },
-        backendOptions: [{ value: "https://api.dingyue.gq/sub?" },{ label: "站长自用",
-                value:
-                  "https://api2.dingyue.gq/sub?"
-              },],
+        backendOptions: [{ value: "https://api.dingyue.gq/sub?" }],
         remoteConfig: [
           {
             label: "universal",
